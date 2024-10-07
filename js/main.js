0fb851e372ea15927ece6c92277220dcbc7b1328
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2024-10-07 22:44:46 +0100

    Added changes to intro text

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Mobile Nav
 let hamburger = document.querySelector("span#hamburger");
 let mobileNavCont = document.querySelector(".nav-wrapper");
 
@@ -6,3 +7,20 @@ hamburger.addEventListener("click", showNav);
 function showNav(){
     mobileNavCont.classList.toggle("show");
 }
+
+// Intro Text
+function revealText(){
+    let introText = document.querySelector("#txt");
+    let playText = document.querySelector(".play-txt");
+
+    if (introText.style.display === "none"){
+        introText.style.display = "block"
+        playText.style.display = "none";
+    }else{
+        introText.style.display = "none";
+        playText.style.display = "block";
+    }
+
+}
+setInterval(revealText, 5000);
+document.querySelector(".play-txt").style.display = "none";
