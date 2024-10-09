00bda4da7d793de0f0c0b2a7b2c883676ee54acd
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2024-10-09 12:04:34 +0100

    Dark-mode functionality enabled

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -13,3 +13,33 @@ document.addEventListener("click", function hideDarkIconBox() {
         iconCont.classList.remove("show");
     }
 });
+
+
+let darkMode = document.getElementById("dk-mode");
+let lightMode = document.getElementById("lt-mode");
+
+// Dark mode 
+darkMode.addEventListener("click", function applyDarkMode(){
+    // Add the class to the body
+    document.body.classList.add("dark-mode");
+    // header
+    document.querySelector("header").classList.add("dark-mode");
+    // Nav
+    document.querySelector("header > nav").classList.add("dark-mode");
+    // Hero
+    document.querySelector(".hero").classList.add("dark-mode");
+}); 
+
+
+// Light Mode
+lightMode.addEventListener("click", function removeDarkMode(){
+        // body
+        document.body.classList.remove("dark-mode");
+        // header
+        document.querySelector("header").classList.remove("dark-mode");
+        // Nav
+        document.querySelector("header > nav").classList.remove("dark-mode");
+        // Hero
+        document.querySelector(".hero").classList.remove("dark-mode");
+       
+});
