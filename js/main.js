a62d19743774daa637cf5117ef5aa29f608a9e9f
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2024-10-14 18:16:03 +0100

    New changes

--- js/main.js
+++ js/main.js
@@ -109,3 +109,25 @@ firstText.style.display = "block";
 secondText.style.display = "none";
 thirdText.style.display = "none"
 setInterval(inspireTextEffect, 3000);
+
+// Contact-section
+const linkedin = document.getElementById("linkedin");
+const gitHub = document.getElementById("github");
+const instagram = document.getElementById("instagram");
+const mail = document.getElementById("email");
+
+linkedin.addEventListener("click", function goToLinkedin(){
+    window.open("https://www.linkedin.com/in/chiemezieuche/", "_blank");
+});
+
+gitHub.addEventListener("click", function openGitHub(){
+    window.open("https://github.com/Chiemezie-Uchenwoke", "_blank");
+});
+
+instagram.addEventListener("click", function openIg(){
+    window.open("https://www.instagram.com/chiemezie_uchenwoke?igsh=MWs4aW9tenBuYnpyMQ==", "_blank");
+});
+
+mail.addEventListener("click", function openMail(){
+    window.open("mailto:uchenwoke.chiemezie@gmail.com", "_blank");
+});
