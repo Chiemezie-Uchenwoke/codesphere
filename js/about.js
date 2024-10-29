7410258d187d0651a0ef5fb7e85a7f784da0e802
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2024-10-29 22:48:00 +0100

    Added an animation to the about hero

--- js/about.js
+++ js/about.js
@@ -3,3 +3,20 @@ const getStarted = document.getElementById("get-started");
 getStarted.addEventListener("click", () => {
     window.location.href = "./blog.html";
 });
+
+// Mission
+/* document.addEventListener("DOMContentLoaded", function () {
+    const missionParagraph = document.querySelector(".mission p");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                missionParagraph.classList.add("animate-text");
+                observer.unobserve(missionParagraph); // Stops observing once animated
+            }
+        });
+    }, { threshold: 0.1 }); // Trigger when 10% of the element is in view
+
+    observer.observe(missionParagraph);
+});
+ */
