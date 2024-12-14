9226e8eb343c7078e5f27b9876ceb231cacdef55
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2024-12-14 17:52:36 +0100

    Remove scroll reveal

--- js/scroll_reveal.js
+++ js/scroll_reveal.js
@@ -1,5 +1,5 @@
 // Index.html
-ScrollReveal().reveal('.featured');
+/* ScrollReveal().reveal('.featured');
 
 ScrollReveal().reveal('.f-grid-box', { delay: 100 });
 ScrollReveal().reveal('.g-grd-1', { delay: 500 });
@@ -7,10 +7,10 @@ ScrollReveal().reveal('.g-grd-2', { delay: 500 });
 ScrollReveal().reveal('.g-grd-3', { delay: 500 });
 ScrollReveal().reveal('.g-grd-4', { delay: 500 });
 ScrollReveal().reveal('.g-grd-5', { delay: 500 });
-ScrollReveal().reveal('.g-grd-6', { delay: 500 });
+ScrollReveal().reveal('.g-grd-6', { delay: 500 }); */
 
 // blog.html
-ScrollReveal().reveal('.cont');
+/* ScrollReveal().reveal('.cont');
 ScrollReveal().reveal('.one', { delay: 500 });
 ScrollReveal().reveal('.two', { delay: 500 });
 ScrollReveal().reveal('.three', { delay: 500 });
@@ -22,4 +22,4 @@ ScrollReveal().reveal('.eight', { delay: 500 });
 ScrollReveal().reveal('.nine', { delay: 500 });
 ScrollReveal().reveal('.ten', { delay: 500 });
 ScrollReveal().reveal('.eleven', { delay: 500 });
-ScrollReveal().reveal('.twelve', { delay: 500 });
+ScrollReveal().reveal('.twelve', { delay: 500 }); */
